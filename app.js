cde42e7af617208b14056fa9b2f398b461279ae9
lalogf <lalogf@gmail.com>
2015-06-03 17:17:35 -0500

    Added ejs-locals to manage layouts. Also gathering data for editing is working

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
-  	app = express();
+  	app = express(),
   	models = require('./models/index'),
+  	methodOverride = require ('method-override'),
+  	engine = require('ejs-locals');
 
 
 app.set("view engine" , "ejs");
+app.engine('ejs', engine);
 
 app.use(bodyParser.urlencoded({
 	extended:true
@@ -12,7 +15,8 @@ app.use(bodyParser.urlencoded({
 
 
 // tell your app to use the module
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded());
+app.use(methodOverride("_method"));
 
 app.get('/', function (req, res){
 	var templateData ={
@@ -40,18 +44,53 @@ app.post('/signup',function (req, res){
 	});
 });
 
+app.get('/edit/:id', function (req, res) {
+	var userId = parseInt(req.params.id, 10);
+	var templateData = {};
+	models.User.findOne({
+			where: {id: userId}
+		}).then(function (user) {
+			templateData.user = user
+		}).finally(function(){
+			res.render('edit.ejs',templateData)
+		});
+});
+
+// app.put('/edit/:id',function (req, res) {
+// 	var userId = parseInt(req.params.id, 10);
+// 	var templateData = {}; 
+// 	models.User.findOne(userId).then(function (user){
+// 		user.updateAttributes({
+// 			first_name: req.body.firstname,
+// 			last_name: req.body.lastname,
+// 			username: req.body.username,
+// 			password: req.body.password
+// 		})
+// 		.success(function (){
+// 			res.redirect('/');
+// 		});
+// 	});
+// });
+// app.delete('/delete/:id', function (req, res){
+// 	console.log(this);
+//     models.User.find(req.params.id).success(function (user){
+//         user.destroy().success(function(){
+//             res.redirect('/');
+//         });
+//     });
+// });
 
 
-// app.post('/carrier', function (req, res){
-//     models.Carrier.create({
-//         carrier_name: req.body.carrier,
-//         country_code: req.body.country,
-//         carrier_logo: req.body.carrierlogo
-//     }).then(function (carrier){
-//         res.redirect('/admin')
-//     },function (error){
-//         req.flash('info', error);
-//         res.redirect('/admin');
+// app.put('/edit/:id', function (req, res){
+//     models.User.find(req.params.id).success(function (user){
+//         user.updateAttributes({
+//             first_name: req.body.firstname,
+//             last_name: req.body.lastname,
+//             age: req.body.age
+//         }) 
+//         .success(function(){
+//             res.redirect("/");
+//         });
 //     });
 // });
 
